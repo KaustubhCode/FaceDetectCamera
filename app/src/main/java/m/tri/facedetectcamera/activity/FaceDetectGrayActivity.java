841664027867833b7402fdd1f1aff6eec569713b
nikhilshenoy <nikhilshenoy>
2018-04-12 16:51:51 +0530

    Revert "Commit"
    This reverts commit bc1d095525bfe14940f1fe982f4397938f0086ee.

--- app/src/main/java/m/tri/facedetectcamera/activity/FaceDetectGrayActivity.java
+++ app/src/main/java/m/tri/facedetectcamera/activity/FaceDetectGrayActivity.java
@@ -29,7 +29,6 @@ import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
 
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -596,7 +595,6 @@ public final class FaceDetectGrayActivity extends AppCompatActivity implements S
             fdet = new android.media.FaceDetector(bmp.getWidth(), bmp.getHeight(), MAX_FACE);
 
             android.media.FaceDetector.Face[] fullResults = new android.media.FaceDetector.Face[MAX_FACE];
-            faceLooking = new boolean[MAX_FACE];
             fdet.findFaces(bmp, fullResults);
 
             for (int i = 0; i < MAX_FACE; i++) {
